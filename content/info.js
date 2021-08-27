9a338ebf6548ba5f14ec45cce946abc3ab44ebd9
flavionn <flavio8163@gmail.com>
2021-08-27 06:52:39 -0300

    update info

--- content/info.js
+++ content/info.js
@@ -1,4 +1,4 @@
 export default {
 	"textHero": "Building fast & useful frontend for cool products",
-	"textAbout": "Hello, I am Flávio. Since I started my career I have been working with Visual Design of things.<br><br>But I've always been interested in technology and development too. So, I worked a few times as a developer and wrote front-end and back-end code for projects.<br><br>In 2021 I decided to focus my career entirely on frontend development.<br><br>At this time I'm looking for a frontend job and bringing life to my own projects, learning and using things like <b>React</b>, <b>Vue</b>, <b>Nuxt</b>, <b>Vite</b> and <b>Windi CSS</b>."
+	"textAbout": "Hello, I am Flávio. Since I started my career I have been working with Visual Design of things.<br><br>But I've always been interested in technology and development too. So, I worked a few times as a developer and wrote front-end and back-end code for projects.<br><br>In 2021 I decided to focus my career entirely on frontend development.<br><br>At this time I'm looking for a frontend job and bringing life to my own projects, practicing things like <b>React</b>, <b>Vue</b>, <b>Nuxt</b>, <b>Vite</b> and <b>Windi CSS</b>."
 }
